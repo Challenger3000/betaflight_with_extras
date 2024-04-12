9f895f2d37757005c3ac23f419d29c767ba517d6
Karlis Martinsons <k.martinsons@fixar.pro>
2024-04-12 22:33:40 +0300

    fixed multiple definitions

--- src/main/flight/mixer.c
+++ src/main/flight/mixer.c
@@ -346,7 +346,7 @@ static void applyFlipOverAfterCrashModeToMotors(void)
     }
 }
 
-int32_t map(int32_t x, int32_t in_min, int32_t in_max, int32_t out_min, int32_t out_max)
+int32_t _map(int32_t x, int32_t in_min, int32_t in_max, int32_t out_min, int32_t out_max)
 {
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
