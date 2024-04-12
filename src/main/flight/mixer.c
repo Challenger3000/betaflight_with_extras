b53e078a2710cd14ed785b6b9b35d865bcaeee19
Karlis Martinsons <k.martinsons@fixar.pro>
2024-04-12 22:35:04 +0300

    correct input variable types

--- src/main/flight/mixer.c
+++ src/main/flight/mixer.c
@@ -346,7 +346,7 @@ static void applyFlipOverAfterCrashModeToMotors(void)
     }
 }
 
-int32_t _map(int32_t x, int32_t in_min, int32_t in_max, int32_t out_min, int32_t out_max)
+int32_t _map(float x, float in_min, float in_max, float out_min, float out_max)
 {
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
